62590f0b4168e9776d075048334da5764ba0908e
Pedro Dias <pdr050203@gmail.com>
2025-02-18 00:04:50 -0300

    feat: add route task, database, server response, utils and middlewares

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import { buildRoutePath } from './utils/build-route-path.js'
+import { Database } from './database.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+]
