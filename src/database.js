2058c00b1d87eaf84fe4798f1524acf871a6bb49
Pedro Dias <pdr050203@gmail.com>
2025-03-02 19:17:49 -0300

    feat: add update task functionality

--- src/database.js
+++ src/database.js
@@ -20,12 +20,14 @@ export class Database {
   }
 
   select(table, search) {
-    let data = [this.#database[table]] ?? [];
+    let data = this.#database[table] ?? []
 
     if (search) {
       data = data.filter(row => {
         return Object.entries(search).some(([key, value]) => {
-          return row[key].toLowerCase().includes(value.toLowerCase())
+          if (!value) return true
+
+          return row[key].includes(value)
         })
       })
     }
@@ -43,6 +45,16 @@ export class Database {
     this.#persist()
 
     return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      const row = this.#database[table][rowIndex]
+      this.#database[table][rowIndex] = { id, ...row, ...data }
+      this.#persist()
+    }
   }
 
   delete(table, id) {
